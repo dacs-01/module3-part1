d6b1d1ffd7103e9c742826e997c14f8de661c78a
dacs-01 <98275383+dacs-01@users.noreply.github.com>
2022-02-01 20:05:18 -0500

    Adding information to Exercise 3

--- ex3.py
+++ ex3.py
@@ -1,3 +1,10 @@
+# Group members for this exercise are Daniel Cisneros, 
+# Dene Logan, Ricky Sequera, and Joseph Onghena.
+
+# This program takes a string input and returns the amount of each
+# character that exists in the string, as well as removing white spaces
+# from being counted and making all characters lowercase.
+
 from collections import Counter
 
 
