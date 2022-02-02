e819288ac2ec1e01a73087fa889532da4bb33735
dacs-01 <98275383+dacs-01@users.noreply.github.com>
2022-02-01 20:14:49 -0500

    adding more info to exercise 1

--- ex1.py
+++ ex1.py
@@ -1,13 +1,17 @@
 # Group members for this exercise are Daniel Cisneros, 
 # Dene Logan, Ricky Sequera, and Joseph Onghena.
 
+# This program will take a hardcoded list and put every number, recurring or 
+# non-recurring, into another new list. In this new list, it will contain one of 
+# each of all numbers in the original list, making it a unique list.
+
 
 def unique_el(nums):
     list = []
-    for i in nums:
-        if i not in list:
-            list.append(i)
-    return list
+    for i in nums: # iterates thru inputted list
+        if i not in list: # if the number is not in new list already...
+            list.append(i) # ...add it to the new list
+    return list # return the new list
 
 my_list = [1,2,3,4,4,5,3,-5,-6,-6]
 
