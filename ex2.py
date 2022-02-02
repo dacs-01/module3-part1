4283fe4cf935105a2189c00e66719e642c3b9d67
dacs-01 <98275383+dacs-01@users.noreply.github.com>
2022-02-01 20:04:04 -0500

    Adding details to exercise 2

--- ex2.py
+++ ex2.py
@@ -1,3 +1,10 @@
+# Group members for this exercise are Daniel Cisneros, 
+# Dene Logan, Ricky Sequera, and Joseph Onghena.
+
+# This program combines hardcoded dictionaries and deleting the keys that exist once.
+# Some of the code was inspired by a program written here:
+# https://stackoverflow.com/questions/57253784/python-program-to-combine-two-dictionary-adding-values-for-common-keys
+
 from typing import Counter
 
 
